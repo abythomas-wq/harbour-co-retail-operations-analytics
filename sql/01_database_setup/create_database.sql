eaa93edd00d0369aef1d4d5053beb97b1a1163e4
abythomas-wq <abythomaspkl@gmail.com>
2026-07-17 10:10:51 +0530

    Add database creation script

---
+++ sql/01_database_setup/create_database.sql
@@ -0,0 +1,8 @@
+-- =====================================================
+-- Harbour & Co. Retail Operations Analytics
+-- Database Creation Script
+-- =====================================================
+
+CREATE DATABASE IF NOT EXISTS harbour_co_retail_analytics;
+
+USE harbour_co_retail_analytics;
