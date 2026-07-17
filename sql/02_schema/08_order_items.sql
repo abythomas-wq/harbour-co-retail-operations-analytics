1cd60cc53ce2a5acb780d8d52af1376df2d48415
abythomas-wq <abythomaspkl@gmail.com>
2026-07-17 10:20:24 +0530

    Create order items table schema

---
+++ sql/02_schema/08_order_items.sql
@@ -0,0 +1,24 @@
+-- =====================================================
+-- Harbour & Co. Retail Operations Analytics
+-- Order Items Table
+-- =====================================================
+
+USE harbour_co_retail_analytics;
+
+CREATE TABLE IF NOT EXISTS order_items (
+    order_item_id INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT NOT NULL,
+    product_id INT NOT NULL,
+    quantity INT NOT NULL,
+    unit_price DECIMAL(10,2) NOT NULL,
+    discount_amount DECIMAL(10,2) DEFAULT 0,
+    line_total DECIMAL(10,2) NOT NULL,
+
+    CONSTRAINT fk_order_items_order
+        FOREIGN KEY (order_id)
+        REFERENCES orders(order_id),
+
+    CONSTRAINT fk_order_items_product
+        FOREIGN KEY (product_id)
+        REFERENCES products(product_id)
+);
